e59644c0059c68cd4acf7260154733786e5dd643
scriubuguri <scriubuguri@tragdate.ninja>
2023-12-15 07:00:12 +0200

    extract poetry URLs

--- poezii.sh
+++ poezii.sh
@@ -2,4 +2,4 @@
 
 #It extracts the poetry content from a website
 
-curl -s "http://poezii.romanianvoice.com/index.php" | pup "tbody tr:nth-child(3) a attr{href}" | head -n2 
+curl -s "http://poezii.romanianvoice.com/index.php" | pup "tbody tr:nth-child(3) a attr{href}" | head -n1 | while read -r author_url; do curl -s "$author_url" | pup "ol li a attr{href}" | sed 's|^..|http://romanianvoice.com/poezii|' | head -n2; done  
