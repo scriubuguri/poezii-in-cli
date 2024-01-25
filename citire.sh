fce2eaa3871c862da3a8ee1b98f5a6fed20d2a99
scriubuguri <scriubuguri@tragdate.ninja>
2024-01-25 21:09:08 +0200

    #1

--- citire.sh
+++ citire.sh
@@ -2,7 +2,4 @@
 
 preview_size=$(($(tput cols) * 40 / 100))
 
-find data -type f | fzf --height 100% --pointer='➤'  --preview 'less {}' --preview-window "$preview_size" --bind 'enter:execute(less {})' --color 'fg:57,fg+:93,bg:234,preview-bg:232,border:201'
-
-
-
+find data -type f | sed 's|^data/||' | fzf --height 100% --pointer='➤' --preview 'less data/{}' --preview-window "$preview_size" --bind 'enter:execute(less data/{})' --color 'fg:57,fg+:93,bg:234,preview-bg:232,border:201'
